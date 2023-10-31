2c2664c15c28d08249723808350924fa2242fe0f
Kostadin Misev <kokimisev@gmail.com>
2023-10-31 07:08:11 +0100

    WebFilter with configuration

--- web/src/main/java/mk/ukim/finki/wp/eshop/web/filter/LoginFilter.java
+++ web/src/main/java/mk/ukim/finki/wp/eshop/web/filter/LoginFilter.java
@@ -2,19 +2,24 @@ package mk.ukim.finki.wp.eshop.web.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import mk.ukim.finki.wp.eshop.model.User;
 
 import java.io.IOException;
 
-@WebFilter
+@WebFilter(filterName="auth-filter",urlPatterns = "/*",
+        dispatcherTypes = {DispatcherType.REQUEST,DispatcherType.FORWARD},
+        initParams = @WebInitParam(name="ignore-path", value="/login"))
 public class LoginFilter implements Filter {
 
+    private String ignorePath;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
+        ignorePath = filterConfig.getInitParameter("ignore-path");
     }
 
     @Override
@@ -23,12 +28,12 @@ public class LoginFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         User user = (User)request.getSession().getAttribute("user");
         String path = request.getServletPath();
-        if (!"/login".equals(path) && user==null) {
-            response.sendRedirect("/login");
-        } else {
+        if (ignorePath.startsWith(path) || user!=null) {
             System.out.println("WebFilter preprocessing...");
             filterChain.doFilter(servletRequest,servletResponse);
             System.out.println("WebFilter postprocessing...");
+        } else {
+            response.sendRedirect("/login");
         }
 
     }
